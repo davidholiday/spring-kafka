a117f4433a7c3e69c0e8e1c7d850a25924659d11
snerd <davidholiday@hush.com>
2023-08-02 11:00:03 -0600

    got spring and kafka talking

--- src/main/java/com/davidholiday/App.java
+++ src/main/java/com/davidholiday/App.java
@@ -1,13 +1,45 @@
 package com.davidholiday;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+@SpringBootApplication
+public class App {
+
+    public static void main( String[] args ) {
+        SpringApplication.run(App.class, args);
     }
+
+
+    @Bean
+    public NewTopic topic() {
+        return TopicBuilder.name("topic1")
+                           .partitions(10)
+                           .replicas(1)
+                           .build();
+    }
+
+
+    @Bean
+    public ApplicationRunner runner(KafkaTemplate<String, String> template) {
+        return args -> {
+            template.send("topic1", "test");
+        };
+    }
+
+
+    @KafkaListener(id = "myId", topics = "topic1")
+    public void listen(String in) {
+        System.out.println(in);
+    }
+
+
 }
