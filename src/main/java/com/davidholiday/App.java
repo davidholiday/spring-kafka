7b1533629bf70fabb556fa6cd7cc56f0a52da3ed
snerd <davidholiday@hush.com>
2023-08-02 12:55:26 -0600

    got multiple threads in kafka going

--- src/main/java/com/davidholiday/App.java
+++ src/main/java/com/davidholiday/App.java
@@ -2,6 +2,10 @@ package com.davidholiday;
 
 
 import org.apache.kafka.clients.admin.NewTopic;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,14 +18,29 @@ import org.springframework.kafka.core.KafkaTemplate;
 @SpringBootApplication
 public class App {
 
+    Logger LOG = LoggerFactory.getLogger(App.class);
+
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
     }
 
 
     @Bean
-    public NewTopic topic() {
-        return TopicBuilder.name("topic1")
+    public NewTopic serializeTopic() {
+        return TopicBuilder.name("serialize")
+                           // *!* you need at least as many partitions as you have consumers
+                           // check "spring.kafka.listener.concurrency" in file application.properties 
+                           .partitions(10)
+                           .replicas(1)
+                           .build();
+    }
+
+
+    @Bean
+    public NewTopic serializeAckTopic() {
+        return TopicBuilder.name("serialize_ack")
+                           // *!* you need at least as many partitions as you have consumers
+                           // check "spring.kafka.listener.concurrency" in file application.properties 
                            .partitions(10)
                            .replicas(1)
                            .build();
@@ -31,14 +50,14 @@ public class App {
     @Bean
     public ApplicationRunner runner(KafkaTemplate<String, String> template) {
         return args -> {
-            template.send("topic1", "test");
+            template.send("serialize_ack", "test");
         };
     }
 
 
-    @KafkaListener(id = "myId", topics = "topic1")
+    @KafkaListener(id = "myId", topics = "serialize_ack")
     public void listen(String in) {
-        System.out.println(in);
+        LOG.info("received ack message: {}", in);
     }
 
 
